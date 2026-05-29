266ec89f5a8ff9025b7e140a08be292e83a97a1d
mina-45b <bmonroy197@gmail.com>
2026-05-29 09:45:28 +0200

    añade funcion perdirNumero y comprobarNumero

--- 22-puerta-correcta/js/main.js
+++ 22-puerta-correcta/js/main.js
@@ -3,8 +3,38 @@ let numero = function() {
     return numero;
 }
 
+function pedirNumero() {
+    //Al hacer el prompt numeroEscogido solo puede ser isNaN, ya no es null ni ""
+    let numeroEscogido = "";
+
+    do {
+
+        numeroEscogido = parseInt(prompt("Elige una opción\n\n|·| Puerta 1 > Introduzca: 1\n|·| Puerta 2 > Introduzca: 2"));
+
+        if(isNaN(numeroEscogido)) {
+            alert("Introduzca un valor válido")
+        } else if(numeroEscogido != 1 && numeroEscogido != 2) {
+            alert("Introduzca un número entre 1 y 2");
+        }
+    }while(isNaN(numeroEscogido) || (numeroEscogido != 1 && numeroEscogido != 2));
+
+    return numeroEscogido;
+}
+
+function comprobarNumero() {
+    let numeroUsuario = pedirNumero();
+
+    if(numeroUsuario === numero()) {
+        alert("Correcto, puedes entrar");
+        return true;
+    } else {
+        alert("Ohh!, has muerto");
+        return false;
+    }
+}
+
 function iniciar() {
-    let numeroEscogido = parseInt(prompt("Elige una opción\n\n|·| Puerta 1 > Introduzca: 1\n|·| Puerta 2 > Introduzca: 2"));
+    let resultado = comprobarNumero();
 }
 
 iniciar();
