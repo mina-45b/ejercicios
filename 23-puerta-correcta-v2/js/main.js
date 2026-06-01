f23a2cbf46c4dd2e4283631cd87518cb8c117b48
mina-45b <bmonroy197@gmail.com>
2026-06-01 13:00:37 +0200

    ejercicio 23 v2 terminado

--- 23-puerta-correcta-v2/js/main.js
+++ 23-puerta-correcta-v2/js/main.js
@@ -16,6 +16,24 @@ const puertas = [
 
 let numeroAleatorio;
 let juegoActivo = true;
+let seguirJugando = true;
+let puertasAbiertas = 0;
+let conteoPuertas = document.getElementById('conteoPuertas');
+
+
+function precargarImagenes() {
+    const imagenes = [
+        'assets/puerta-cerrada.png',
+        'assets/puerta-abierta.png',
+        'assets/ganar.png',
+        'assets/perder.png'
+    ];
+
+    imagenes.forEach(src => {
+        const imag = new Image();
+        imag.src = src;
+    });
+}
 
 function jugar() {
     numeroAleatorio = generarNumero();
@@ -44,14 +62,23 @@ function clickPuerta(numero, imagen, otraPuerta) {
     juegoActivo = false;
     otraPuerta.style.display = 'none';
 
+
+
     if (numero === numeroAleatorio) {
         imagen.src = 'assets/ganar.png';
+        puertasAbiertas++;
+
+        conteoPuertas.textContent = puertasAbiertas;
 
         setTimeout(() => {
             nuevaRonda();
         }, 2000);
     } else {
         imagen.src = 'assets/perder.png';
+
+        setTimeout(() => {
+            repetirJuego();
+        }, 2000);
     }
 
     console.log(numero + " este es el numero de puerta");
@@ -73,10 +100,44 @@ function nuevaRonda() {
 }
 
 
+const contenedorPregunta = document.querySelector('.pregunta');
+const contenedorPuertas = document.querySelector('#contenedor-puertas');
+const contadorPuertas =  document.getElementById('abiertas');
+
+const btnSi = document.getElementById('si');
+const btnNo = document.getElementById('no');
+
+btnSi.addEventListener('click', () => {
+    contenedorPuertas.style.display = 'grid';
+    contadorPuertas.style.display = 'block';
+    contenedorPregunta.style.display = 'none';
+    contenedorPuertas.classList.replace('puertas-nueva', 'puertas');
+    
+    puertasAbiertas = 0;
+
+    conteoPuertas.textContent = puertasAbiertas;
+    nuevaRonda();
+});
+
+btnNo.addEventListener('click', () => {
+    contenedorPuertas.style.display = 'block';
+    contadorPuertas.style.display = 'block';
+    contenedorPregunta.style.display = 'none';
+});
+
+function repetirJuego() {
+    contenedorPregunta.style.display = 'block';
+    contenedorPuertas.style.display = 'none';
+    contadorPuertas.style.display = 'none';
+
+}
+
+
 function generarNumero() {
     let numero = Math.floor(Math.random() * 2)+1;
     return numero;
 }
 
+precargarImagenes();
 jugar();
 
