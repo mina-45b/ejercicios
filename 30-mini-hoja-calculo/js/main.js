b87fc106ff14f3ca29babbe9588c0639cc3477f3
mina-45b <bmonroy197@gmail.com>
2026-06-04 13:47:05 +0200

    Version1 funciona

--- 30-mini-hoja-calculo/js/main.js
+++ 30-mini-hoja-calculo/js/main.js
@@ -34,7 +34,7 @@ function pedirNumeros() {
     let abc = [[], [], []];
     let numeroUsuario = 0;
 
-    for(let i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) {
 
         numeroUsuario = parseInt(prompt(`Introduzca 9 números\nLlevas ${i}/9`));
 
@@ -52,9 +52,52 @@ function pedirNumeros() {
 }
 
 
-const a = document.querySelectorAll('.a');
-const b = document.querySelectorAll('.b');
-const c = document.querySelectorAll('.c');
-const d = document.querySelectorAll('.d');
+let abc = pedirNumeros();
 
-console.log(a);
+const filas = [
+    document.querySelectorAll('.row1'),
+    document.querySelectorAll('.row2'),
+    document.querySelectorAll('.row3')
+];
+
+let suma = 0;
+let resultado = new Array();
+
+//Recorrer cada .rowx y recorre dentro de cada .rowx los elementos que tienen la misma clase
+filas.forEach((fila, filaIndex) => { // 0, 1, 2, 3
+    fila.forEach((columna, colIndex) => { //0-1-2 , 3-4-5, 6-7-8, 9-10-11
+        if(colIndex < 3){
+            columna.textContent = abc[filaIndex][colIndex]; //(1-4-5) = [0], 7-0-52-1-9
+
+            suma += abc[filaIndex][colIndex]; // 0+1=1 - 1+4=5 - 5+5=10
+        }
+
+
+        if(colIndex == 2) {
+            resultado.push(suma);
+            suma = 0;
+        }
+
+        if(colIndex == 3) {
+            columna.textContent = resultado[filaIndex];
+        }
+
+        console.log(columna.textContent + " El contenido");
+        console.log(colIndex + " Columna indice");
+        console.log(filaIndex + " Fila indice")
+
+        console.log(suma + " La suma");
+        /*if(filaIndex === 3) {
+            console.log("hola");
+
+        }
+        let fila = Math.floor(i / 3);
+        abc[fila].push(numeroUsuario);
+        
+        */
+
+    });
+});
+
+
+
